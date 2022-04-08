bc3b898d5f0956d22277b0c2fa2ee7e1ad989bde
chrisjusel <christianvit@hotmail.it>
2022-04-08 17:37:24 +0200

    fix

--- script.js
+++ script.js
@@ -82,10 +82,7 @@ function shuffle(array) {
         location.reload();
     });
 
-    $(".playAgain").on('click', () => {
+        $(".playAgain").on('click', () => {
         location.reload();
     });
-<<<<<<< HEAD
 
-=======
->>>>>>> 2e77da19516e5ec70fdf4d0818235be880a70efb
